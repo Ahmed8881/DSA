006ebc295b707ce3d69e099ea531d88d9a749b9e
Ahmed8881 <buttahmed2004@gmail.com>
2024-09-07 23:39:45 +0500

    CHAGNES

--- lab1/Problem2.py
+++ lab1/Problem2.py
@@ -16,6 +16,5 @@ def SearchIndex(arr,x):
             
         
 array1=[0, 1, 2, 2, 5, 6, 7, 8, 9]
-array=[22,2,1,7,11,13,5,2,9]
 a=int(input("Enter a num: "))
 print(SearchIndex(array1,a))        
