2fe09379851c0ad30658eae22ebb029075c907cd
Ahmed881 <buttahmed2004@gmail.com>
2024-09-18 16:04:45 +0500

    Refactor file output in Practice.py to use matplotlib.pyplot for improved visualization

--- lab 3/Scrapping.py
+++ lab 3/Scrapping.py
@@ -1,19 +1,8 @@
-# import matplotlib.pyplot as plt
-# import pandas as pd
-# df=pd.read_csv('data.csv')
-# print(df.dtypes)
-# list1=df['Name'].values.tolist()
-# list2=df['Age'].values.tolist()
-# plt.bar(list1,list2)
-# plt.show()
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 from selenium.webdriver.chrome.service import Service
 import time as sleep
-
-# webdriver can be downloaded from
-# https://sites.google.com/chromium.org/driver/downloads/versionselection?authuser=0
 
 service = Service(
     executable_path="C:/Users/butta/chromedriver-win64/chromedriver-win64/chromedriver.exe"
@@ -26,15 +15,16 @@ products = []  # List to store name of the product
 prices = []  # List to store price of the product
 # ratings = []  # List to store rating of the product
 
-driver.get("https://www.whatmobile.com.pk/")
+driver.get("https://www.hamzastore.pk/")
 
 content = driver.page_source
 
 soup = BeautifulSoup(content, features="html.parser")
 # print(soup)
-for a in soup.findAll("div", attrs={"class": "quick-buttons "}):
-    print(a)
-    name = a.find("a", attrs={"class": "BiggerText"})
+for a in soup.findAll("div", attrs={"class": "deal-box"}):
+    # print(a)
+    # name = a.find("a", attrs={"class": "BiggerText"})
+    name = a.find("p", attrs={"class": " roboto-700 "})
     price = a.find("span", attrs={"class": "PriceFont"})
     if name != None and price != None:
         products.append(name["title"])
@@ -43,4 +33,6 @@ for a in soup.findAll("div", attrs={"class": "quick-buttons "}):
         break
 
 df = pd.DataFrame({"Product Name": products, "Price": prices})
-df.to_csv("product.csv", index=False, encoding="utf-8")
+df.to_csv("item-box.csv", index=False, encoding="utf-8")
+
+
