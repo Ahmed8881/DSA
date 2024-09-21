82d4a5af3bfef66cbf356f4b9c4ecf2020888986
Ahmed881 <buttahmed2004@gmail.com>
2024-09-21 09:56:49 +0500

    Refactor sorting algorithms and matrix operations
    - Removed unnecessary code files for Bubble Sort, Hybrid Merge Sort, Insertion Sort, Merge Sort, Selection Sort, and utility functions.
    - Updated the printMatrix function to handle starting index, rows, and columns.
    - Added MatAdd and MatAddPartial functions for matrix addition.
    - Added MatMul function for matrix multiplication.

--- lab3/Part3.py
+++ lab3/Part3.py
@@ -1,16 +1,79 @@
-def printMatrix(A,starting_index,rows,columns):
-    list1=[]
-    for i in range(starting_index[0],rows):
-        for j in range(starting_index[1],columns):
-            list1.append(A[i][j])
-    return list1
+import numpy as np
 
 
+# PROBLEM 1
+def printMatrix(A, starting_index, rows, columns):    
+    for i in range(starting_index[0], rows):
+        if not (0 <= starting_index[0] < rows and 0 <= starting_index[1] < columns):
+            break
+        for j in range(starting_index[1], columns):
+            print(A[i][j], end=" ")
+        print()
+        
+# Problem 2
+def MatAdd(A, B):
+    Sum = []
+    if len(A) == len(B) and len(A[0]) == len(B[0]):
+        for i in range(len(A)):
+            row = []
+            for j in range(len(A[0])):
+                row.append(A[i][j] + B[i][j])
+            Sum.append(row)
+    return Sum
+       
+def MatAddPartial(A, B, start, size):
+    result = np.zeros((size, size))
+    if not (start[0] + size > len(A) or start[1] + size > len(A[0]) or 
+        start[0] + size > len(B) or start[1] + size > len(B[0])):
+            for i in range(size):
+                for j in range(size):
+                    result[i][j] = A[start[0]+i][start[1]+j] + B[start[0]+i][start[1]+j]
+    
+            return result
+        
+def MatMul(A, B):
+    if len(A[0]) == len(B):
+        result = []
+        for i in range(len(A)):
+            row = []
+            for j in range(len(B[0])):
+                sum = 0
+                for k in range(len(B)):
+                    sum += A[i][k] * B[k][j]
+                row.append(sum)
+            result.append(row)
+        return result
+# if __name__ == "__main__":
+    # Problem 1
+    # A=[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+    # printMatrix(A,[1,2],4,4)
+    #Problem 2
+#    A=[[1,2,3],[4,5,6],[7,8,9]]
+#    B=[[9,8,7],[6,5,4],[3,2,1]]
+#    print(MatAddPartial(A,B,[1,0],2))
+# A = [
+#     [1, 2, 3, 4, 5],
+#     [6, 7, 8, 9, 10],
+#     [11, 12, 13, 14, 15],
+#     [16, 17, 18, 19, 20],
+#     [21, 22, 23, 24, 25]
+# ]
 
-# driver code
-A=[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15]]
-# Starting index is a tuple
-starting_index = (0, 3)
-rows=3
-columns=4
-print(printMatrix(A,starting_index,rows,columns))
+# B = [
+#     [1, 1, 1, 1, 1],
+#     [2, 2, 2, 2, 2],
+#     [3, 3, 3, 3, 3],
+#     [4, 4, 4, 4, 4],
+#     [5, 5, 5, 5, 5]
+# ]
+
+# start = (2, 3)
+# size = 2
+
+# result = MatAddPartial(A, B, start, size)
+# print(result)
+
+# Driver fir mul
+A=[[2,3],[4,5]]
+B=[[1],[3]]
+print(MatMul(A,B))  
