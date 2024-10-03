adc3998e2a5a9396b4cc15385311d99398c4b984
Ahmed8881 <buttahmed2004@gmail.com>
2024-10-03 22:10:55 +0500

    Lab 5 added

--- lab5/Problem1.cpp
+++ lab5/Problem1.cpp
@@ -1,5 +1,42 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
+#include<string>
 using namespace std;
+
+void printOptions();
+
 int main(){
-    cout<<"Hello World!"<<endl;
+    int choice=0;
+    vector<string> strings;
+    while(choice!=3){
+    system("cls");
+
+        printOptions();
+        cin>>choice;
+        if(choice==1){
+            string str;
+            cout<<"Enter a string: ";
+            cin>>str;
+            strings.push_back(str);
+            cout << "String added." << endl;
+        } else if(choice==2){
+            if(!strings.empty()){
+                strings.pop_back();
+                cout<<"Last string removed."<<endl;
+            } else {
+                cout<<"No strings to remove."<<endl;
+            }
+        }
+        cout << "Size: " << strings.size() << ", Capacity: " << strings.capacity() << endl;
+        cout << "Press Enter to continue...";
+        cin.ignore();
+        cin.get();
+    }
+    
+}
+void printOptions(){
+    cout<<"1-Add String"<<endl;
+    cout<<"2-Remove String"<<endl;
+    cout<<"3-Exit"<<endl;
+    cout<<"Enter your choice: ";
 }
